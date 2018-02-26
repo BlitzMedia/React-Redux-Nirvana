39297cfdad4fed46d70f10d62a727b634479f54a
faceyspacey <james@faceyspacey.com>
2018-02-26 12:12:40 -0800

    parent child communication part 1 + controlled inputs

--- src/assignments/1/assignment1.js
+++ src/assignments/1/assignment1.js
@@ -3,14 +3,29 @@ import './a1.css'
 
 import PairParent from './bonus/PairParent'
 
+function updateTotal2() {}
+
 class Assignment1 extends Component {
+  constructor(props, context) {
+    super(props, context)
+
+    this.state = {
+      total: 0,
+    }
+  }
+  
+  updateTotal = (value) => {
+    this.setState({ total: this.state.total + value })
+  }
+
   render() {
     return (
       <div>
         <section className="firstComponent">
           <h2>Assignment 1</h2>
-
-          <MyFirstClassComponent />
+          <h1>TOTAL: {this.state.total}</h1>
+          <Child onChange={this.updateTotal} />
+          <Child onChange={this.updateTotal} />
         </section>
 
         <section className="secondComponent">
@@ -23,57 +38,47 @@ class Assignment1 extends Component {
   }
 }
 
-const MyFirstFunctionalComponent = (props) => <h3>{props.num * props.multiplier}</h3>
-
-class MyFirstClassComponent extends React.Component {
+class Child extends React.Component {
   constructor() {
-    super();
+    super()
     // fill this in
 
     this.state = {
+      num: 100,
+      value: '1',
       multiplier: 1
     }
   }
 
-  multiply = () => { // figure out why this method looks different than render and constructor
-    // This looks different because Component methods don't autobind to themselves.
-    // If it wasn't written this way, we would have to bind them manually in the constructor, after super();, like so:
-    // this.multiply = this.multiply.bind(this)
-
-    // fill this in (clue: it uses `this.setState`)
-    // We are not sure what the multiplier should be, so we're multiplying it by itself.
-
-    var newVal = this.state.multiplier * this.state.multiplier
-
-    this.setState({
-      multiplier: newVal
-    })
+  multiply = (event) => {
+    this.setState({ multiplier: this.state.value })
+    const childSubTotal = this.state.num * this.state.value
+    this.props.onChange(childSubTotal)
+    event.preventDefault()
   }
 
   render() {
     return (
       <div>
-        <MyFirstFunctionalComponent num={100} multiplier={this.state.multiplier} />
+        <h3>{this.state.num * this.state.multiplier}</h3>
 
-        <form onSubmit={this.handleSubmit}>
-          <input type="number" required ref="newMultiplier" />
-          <input type="submit" value="Give me a multiplier" />
-        </form>
+        <input
+          type="number"
+          value={this.state.value}
+          onChange={(event) => {
+            this.setState({ value: event.target.value })
+          }}
+        />
 
-        <br/>
-
-        <button onClick={this.multiply} id="multiply">MULTIPLY!</button>
+        <button onClick={this.multiply}>MULTIPLY</button>
       </div>
     )
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    var newMultiplier = this.refs.newMultiplier.value;
-    this.setState({
-      multiplier: newMultiplier
-    });
   }
 }
 
 export default Assignment1;
+
+
+// https://gist.github.com/faceyspacey/229319b9b7cc9c07188e6d9f313bafef
+
+

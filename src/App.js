57724c696fd8d8b233de7021408e0a060ee6aef6
gorkamolero <gorka.molero@gmail.com>
2018-03-08 13:31:57 +0100

    Final version of Framework. Moving things to proper places too

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './App.css';
+import './assets/css/App.css';
 
 import Home from './home'
 import Assignment1 from './assignments/1/assignment1'

11f3cc923eda909c19b83c34841c625f974bff6e
faceyspacey <james@faceyspacey.com>
2018-03-13 03:51:28 -0700

    james changes assignment 2b

--- src/assignments/2/a/assignment2a.js
+++ src/assignments/2/a/assignment2a.js
@@ -17,11 +17,11 @@ function toFahrenheit(celsius) { return (celsius * 9 / 5) + 32; }
 
 function tryConvert(temperature, convert) {
   const input = parseFloat(temperature);
-  if (Number.isNaN(input)) return '';
+  if (isNaN(input)) return '';
 
   const output = convert(input);
   const rounded = Math.round(output * 1000) / 1000;
-  return rounded.toString();
+  return rounded;
 }
 
 
